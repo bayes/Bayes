732c809ca41cf4b304d171e9a0fdb246a0bf07ec
lbretthorst <kmarutyan@pandora.com>
2015-11-25 10:50:55 -0800

    removing comments

--- src/bayes/BayesView.java
+++ src/bayes/BayesView.java
@@ -73,7 +73,6 @@ public class BayesView extends FrameView implements
     private javax.swing.JRadioButtonMenuItem selectedPackageMenutItem;
     public static boolean debug = false;
 
-//BayesView
     public BayesView(SingleFrameApplication app) {
         super(app);
 
